fcca211b54333a6a51bae3be2cb35ed13f3d7715
Jonathan Klinger <jo@cfmmedia.de>
2024-06-30 23:42:59 +0200

    add 2 player round detection

--- mariogo/analyzer/rounds.go
+++ mariogo/analyzer/rounds.go
@@ -6,22 +6,64 @@ import (
 	"time"
 )
 
+var RoundPixels = [][]pixel.Pixel{
+	pixel.RoundOne,
+	pixel.RoundTwo,
+	pixel.RoundThree,
+	pixel.RoundFour,
+	pixel.RoundFive,
+	pixel.RoundSix,
+	pixel.RoundSeven,
+}
+
+var RoundPixels2P = [][]pixel.Pixel{
+	pixel.RoundOne2P,
+	pixel.RoundTwo2P,
+	pixel.RoundThree2P,
+	pixel.RoundFour2P,
+	pixel.RoundFive2P,
+	pixel.RoundSix2P,
+	pixel.RoundSeven2P,
+}
+
+var RoundReferencePixels = []pixel.Pixel{
+	{X: 140, Y: 316},
+	{X: 1204, Y: 316},
+	{X: 140, Y: 676},
+	{X: 1204, Y: 676},
+}
+
+var RoundReferencePixels2P = []pixel.Pixel{
+	{X: 163, Y: 678},
+	{X: 803, Y: 678},
+}
+
+var DoneReferencePixels = []pixel.Pixel{
+	{X: 203, Y: 121},
+	{X: 843, Y: 121},
+	{X: 203, Y: 480},
+	{X: 843, Y: 480},
+}
+
+var DoneReferencePixels2P = []pixel.Pixel{
+	{X: 198, Y: 345},
+	{X: 838, Y: 345},
+}
+
 func (ga *GameAnalyzer) AnalyzeRounds() {
 
 	ga.capture.MatchSetting(0.92, 0.1)
 
-	roundReverencePixels := []pixel.Pixel{
-		{X: 140, Y: 316},
-		{X: 1204, Y: 316},
-		{X: 140, Y: 676},
-		{X: 1204, Y: 676},
-	}
+	roundPixels := RoundPixels
+	roundReferencePixels := RoundReferencePixels
+	doneReferencePixels := DoneReferencePixels
+	donePixel := pixel.Done
 
-	doneReverencePixels := []pixel.Pixel{
-		{X: 203, Y: 121},
-		{X: 843, Y: 121},
-		{X: 203, Y: 480},
-		{X: 843, Y: 480},
+	if ga.playerCount == 2 {
+		roundPixels = RoundPixels2P
+		roundReferencePixels = RoundReferencePixels2P
+		doneReferencePixels = DoneReferencePixels2P
+		donePixel = pixel.Done2P
 	}
 
 	now := time.Now()
@@ -33,35 +75,35 @@ func (ga *GameAnalyzer) AnalyzeRounds() {
 	}
 
 	for i := 0; i < ga.playerCount; i++ {
-		ref := roundReverencePixels[i]
+		ref := roundReferencePixels[i]
 
 		newRound := ga.playerRounds[i]
-		if ga.playerRounds[i] == 0 && ga.capture.Matches(pixel.GetAbsolutePixels(ref, pixel.RoundOne)) {
+		if ga.playerRounds[i] == 0 && ga.capture.Matches(pixel.GetAbsolutePixels(ref, roundPixels[0])) {
 			newRound = 1
 			ga.initRoundTimes()
 		}
-		if ga.playerRounds[i] == 1 && ga.capture.Matches(pixel.GetAbsolutePixels(ref, pixel.RoundTwo)) {
+		if ga.playerRounds[i] == 1 && ga.capture.Matches(pixel.GetAbsolutePixels(ref, roundPixels[1])) {
 			newRound = 2
 		}
-		if ga.playerRounds[i] == 2 && ga.capture.Matches(pixel.GetAbsolutePixels(ref, pixel.RoundThree)) {
+		if ga.playerRounds[i] == 2 && ga.capture.Matches(pixel.GetAbsolutePixels(ref, roundPixels[2])) {
 			newRound = 3
 		}
-		if ga.playerRounds[i] == 3 && ga.capture.Matches(pixel.GetAbsolutePixels(ref, pixel.RoundFour)) {
+		if ga.playerRounds[i] == 3 && ga.capture.Matches(pixel.GetAbsolutePixels(ref, roundPixels[3])) {
 			newRound = 4
 		}
-		if ga.playerRounds[i] == 4 && ga.capture.Matches(pixel.GetAbsolutePixels(ref, pixel.RoundFive)) {
+		if ga.playerRounds[i] == 4 && ga.capture.Matches(pixel.GetAbsolutePixels(ref, roundPixels[4])) {
 			newRound = 5
 		}
-		if ga.playerRounds[i] == 5 && ga.capture.Matches(pixel.GetAbsolutePixels(ref, pixel.RoundSix)) {
+		if ga.playerRounds[i] == 5 && ga.capture.Matches(pixel.GetAbsolutePixels(ref, roundPixels[5])) {
 			newRound = 6
 		}
-		if ga.playerRounds[i] == 6 && ga.capture.Matches(pixel.GetAbsolutePixels(ref, pixel.RoundSeven)) {
+		if ga.playerRounds[i] == 6 && ga.capture.Matches(pixel.GetAbsolutePixels(ref, roundPixels[6])) {
 			newRound = 7
 		}
 
 		done := false
 		if newRound > 0 {
-			done = ga.capture.Matches(pixel.GetAbsolutePixels(doneReverencePixels[i], pixel.Done))
+			done = ga.capture.Matches(pixel.GetAbsolutePixels(doneReferencePixels[i], donePixel))
 		}
 
 		if newRound != ga.playerRounds[i] || done {
---
+++ mariogo/pixel/rounds2p.go
@@ -0,0 +1,118 @@
+package pixel
+
+import (
+	"image/color"
+)
+
+var RoundOne2P = []Pixel{
+	{X: 0, Y: -6, C: color.RGBA{254, 250, 250, 255}},
+	{X: 0, Y: -3, C: color.RGBA{237, 241, 238, 255}},
+	{X: -1, Y: 4, C: color.RGBA{229, 221, 219, 255}},
+	{X: -2, Y: 6, C: color.RGBA{219, 218, 213, 255}},
+}
+
+var RoundTwo2P = []Pixel{
+	{X: -1, Y: -9, C: color.RGBA{252, 251, 253, 255}},
+	{X: 2, Y: -9, C: color.RGBA{249, 252, 252, 255}},
+	{X: 6, Y: -6, C: color.RGBA{244, 246, 248, 255}},
+	{X: 6, Y: -3, C: color.RGBA{234, 236, 236, 255}},
+	{X: 1, Y: 0, C: color.RGBA{230, 232, 228, 255}},
+	{X: -2, Y: 0, C: color.RGBA{231, 231, 228, 255}},
+	{X: -6, Y: 3, C: color.RGBA{228, 227, 225, 255}},
+	{X: -7, Y: 6, C: color.RGBA{227, 220, 220, 255}},
+	{X: -3, Y: 9, C: color.RGBA{215, 211, 210, 255}},
+	{X: 0, Y: 9, C: color.RGBA{205, 210, 206, 255}},
+}
+
+var RoundThree2P = []Pixel{
+	{X: -1, Y: -9, C: color.RGBA{253, 255, 246, 255}},
+	{X: 3, Y: -9, C: color.RGBA{251, 252, 251, 255}},
+	{X: 6, Y: -6, C: color.RGBA{247, 243, 241, 255}},
+	{X: 6, Y: -3, C: color.RGBA{243, 239, 236, 255}},
+	{X: 2, Y: 0, C: color.RGBA{232, 235, 233, 255}},
+	{X: -2, Y: 0, C: color.RGBA{238, 230, 235, 255}},
+	{X: 5, Y: 3, C: color.RGBA{232, 236, 244, 255}},
+	{X: 4, Y: 6, C: color.RGBA{218, 228, 229, 255}},
+	{X: 1, Y: 9, C: color.RGBA{207, 212, 213, 255}},
+	{X: -3, Y: 9, C: color.RGBA{216, 207, 218, 255}},
+}
+
+var RoundFour2P = []Pixel{
+	{X: -5, Y: -6, C: color.RGBA{242, 244, 237, 255}},
+	{X: -5, Y: -3, C: color.RGBA{243, 241, 249, 255}},
+	{X: -3, Y: 0, C: color.RGBA{234, 228, 227, 255}},
+	{X: 2, Y: 0, C: color.RGBA{235, 237, 232, 255}},
+	{X: 6, Y: -6, C: color.RGBA{245, 246, 249, 255}},
+	{X: 5, Y: -3, C: color.RGBA{244, 248, 245, 255}},
+	{X: 4, Y: 4, C: color.RGBA{218, 217, 231, 255}},
+	{X: 4, Y: 7, C: color.RGBA{210, 213, 221, 255}},
+}
+
+var RoundFive2P = []Pixel{
+	{X: 3, Y: -9, C: color.RGBA{247, 244, 243, 255}},
+	{X: -2, Y: -9, C: color.RGBA{246, 248, 249, 255}},
+	{X: -5, Y: -6, C: color.RGBA{242, 246, 250, 255}},
+	{X: -5, Y: -3, C: color.RGBA{242, 238, 237, 255}},
+	{X: -3, Y: 0, C: color.RGBA{228, 232, 233, 255}},
+	{X: 2, Y: 0, C: color.RGBA{238, 232, 229, 255}},
+	{X: 4, Y: 3, C: color.RGBA{213, 223, 227, 255}},
+	{X: 4, Y: 6, C: color.RGBA{210, 220, 226, 255}},
+	{X: 1, Y: 9, C: color.RGBA{206, 211, 216, 255}},
+	{X: -3, Y: 9, C: color.RGBA{215, 210, 202, 255}},
+}
+
+var RoundSix2P = []Pixel{
+	{X: 3, Y: -9, C: color.RGBA{248, 245, 242, 255}},
+	{X: -1, Y: -9, C: color.RGBA{246, 252, 245, 255}},
+	{X: -5, Y: -6, C: color.RGBA{245, 244, 248, 255}},
+	{X: -5, Y: -3, C: color.RGBA{242, 242, 238, 255}},
+	{X: -2, Y: 0, C: color.RGBA{227, 234, 219, 255}},
+	{X: 2, Y: 0, C: color.RGBA{240, 231, 242, 255}},
+	{X: 4, Y: 3, C: color.RGBA{211, 222, 223, 255}},
+	{X: 4, Y: 6, C: color.RGBA{221, 229, 228, 255}},
+	{X: 1, Y: 9, C: color.RGBA{205, 212, 212, 255}},
+	{X: -3, Y: 9, C: color.RGBA{215, 206, 214, 255}},
+	{X: -7, Y: 6, C: color.RGBA{219, 221, 228, 255}},
+	{X: -7, Y: 3, C: color.RGBA{222, 236, 240, 255}},
+}
+
+var RoundSeven2P = []Pixel{
+	{X: -1, Y: -9, C: color.RGBA{255, 255, 248, 255}},
+	{X: 3, Y: -9, C: color.RGBA{255, 255, 250, 255}},
+	{X: 6, Y: -6, C: color.RGBA{247, 247, 234, 255}},
+	{X: 5, Y: -3, C: color.RGBA{241, 247, 252, 255}},
+	{X: 4, Y: 3, C: color.RGBA{217, 224, 230, 255}},
+	{X: 4, Y: 6, C: color.RGBA{226, 228, 216, 255}},
+}
+
+var Done2P = []Pixel{
+	{X: -82, Y: -48, C: color.RGBA{244, 255, 252, 255}},
+	{X: -84, Y: 13, C: color.RGBA{255, 252, 253, 255}},
+	{X: -52, Y: -29, C: color.RGBA{252, 251, 249, 255}},
+	{X: -23, Y: -57, C: color.RGBA{253, 253, 252, 255}},
+	{X: -13, Y: -44, C: color.RGBA{253, 250, 250, 255}},
+	{X: -27, Y: -1, C: color.RGBA{254, 255, 255, 255}},
+	{X: 5, Y: -2, C: color.RGBA{252, 251, 251, 255}},
+	{X: 9, Y: -23, C: color.RGBA{248, 255, 243, 255}},
+	{X: 44, Y: -51, C: color.RGBA{252, 249, 252, 255}},
+	{X: 93, Y: -50, C: color.RGBA{252, 248, 255, 255}},
+	{X: 89, Y: -9, C: color.RGBA{247, 255, 254, 255}},
+	{X: 64, Y: 4, C: color.RGBA{252, 253, 252, 255}},
+	{X: -68, Y: -39, C: color.RGBA{255, 221, 0, 255}},
+	{X: -70, Y: 4, C: color.RGBA{255, 195, 0, 255}},
+	{X: -44, Y: -14, C: color.RGBA{255, 196, 0, 255}},
+	{X: -42, Y: -40, C: color.RGBA{255, 218, 0, 255}},
+	{X: -6, Y: -32, C: color.RGBA{255, 214, 0, 255}},
+	{X: -11, Y: 2, C: color.RGBA{255, 168, 0, 255}},
+	{X: 11, Y: -13, C: color.RGBA{255, 207, 0, 255}},
+	{X: 56, Y: -36, C: color.RGBA{255, 213, 0, 255}},
+	{X: 53, Y: 0, C: color.RGBA{255, 188, 0, 255}},
+	{X: 70, Y: -5, C: color.RGBA{255, 207, 0, 255}},
+	{X: 94, Y: -29, C: color.RGBA{255, 219, 0, 255}},
+	{X: 111, Y: -45, C: color.RGBA{247, 254, 241, 255}},
+	{X: 109, Y: -22, C: color.RGBA{254, 253, 253, 255}},
+	{X: 159, Y: -26, C: color.RGBA{253, 252, 248, 255}},
+	{X: 146, Y: 12, C: color.RGBA{255, 173, 0, 255}},
+	{X: 145, Y: -36, C: color.RGBA{255, 220, 0, 255}},
+	{X: 125, Y: -38, C: color.RGBA{255, 215, 0, 255}},
+}
