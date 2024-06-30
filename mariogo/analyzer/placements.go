b150838b681b42f6207ff3e4608d13c03ae21d34
Jonathan Klinger <jo@cfmmedia.de>
2024-06-30 20:09:47 +0200

    placement detection 2 player

--- mariogo/analyzer/placements.go
+++ mariogo/analyzer/placements.go
@@ -10,7 +10,15 @@ import (
 	"gocv.io/x/gocv"
 )
 
-var placementHashes [12][2]goimagehash.ImageHash
+type OffsetPoints struct {
+	X1 int
+	Y1 int
+	X2 int
+	Y2 int
+}
+
+var PlacementHashes [12][2]goimagehash.ImageHash
+var PlacementHashes2P [12][2]goimagehash.ImageHash
 
 var PlacementReference = [4]pixel.Pixel{
 	{X: 75, Y: 250},
@@ -19,21 +27,31 @@ var PlacementReference = [4]pixel.Pixel{
 	{X: 1185, Y: 610},
 }
 
+var Offset = OffsetPoints{X1: -12, Y1: -10, X2: 20, Y2: 40}
+
+var PlacementReference2Player = [4]pixel.Pixel{
+	{X: 531, Y: 636},
+	{X: 1171, Y: 636},
+}
+
+var Offset2P = OffsetPoints{X1: -15, Y1: -16, X2: 26, Y2: 49}
+
 func GeneratePlacmentHashes() {
 
 	for i := 1; i <= 12; i++ {
 		img := gocv.IMRead(fmt.Sprintf("mariogo/samples/placements/%v.png", i), gocv.IMReadColor)
 		imgDark := gocv.IMRead(fmt.Sprintf("mariogo/samples/placements/%v_dark.png", i), gocv.IMReadColor)
+		img2P := gocv.IMRead(fmt.Sprintf("mariogo/samples/placements2player/%v.png", i), gocv.IMReadColor)
+		img2PDark := gocv.IMRead(fmt.Sprintf("mariogo/samples/placements2player/%v_dark.png", i), gocv.IMReadColor)
 		defer img.Close()
 		defer imgDark.Close()
+		defer img2P.Close()
+		defer img2PDark.Close()
 
-		if img.Empty() || imgDark.Empty() {
+		if img.Empty() || imgDark.Empty() || img2P.Empty() || img2PDark.Empty() {
 			fmt.Println("Fehler beim Einlesen des Bildes")
 			return
 		}
-
-		gocv.CvtColor(img, &img, gocv.ColorBGRToGray)
-		gocv.CvtColor(imgDark, &imgDark, gocv.ColorBGRToGray)
 
 		image, _ := img.ToImage()
 		refImgHash, _ := goimagehash.PerceptionHash(image)
@@ -41,25 +59,43 @@ func GeneratePlacmentHashes() {
 		imageDark, _ := imgDark.ToImage()
 		refImgHashDark, _ := goimagehash.PerceptionHash(imageDark)
 
-		placementHashes[i-1] = [2]goimagehash.ImageHash{*refImgHash, *refImgHashDark}
+		PlacementHashes[i-1] = [2]goimagehash.ImageHash{*refImgHash, *refImgHashDark}
+
+		image2P, _ := img2P.ToImage()
+		refImgHash2P, _ := goimagehash.PerceptionHash(image2P)
+
+		image2PDark, _ := img2PDark.ToImage()
+		refImgHash2PDark, _ := goimagehash.PerceptionHash(image2PDark)
+
+		PlacementHashes2P[i-1] = [2]goimagehash.ImageHash{*refImgHash2P, *refImgHash2PDark}
 	}
 
 }
 
 func (ga *GameAnalyzer) AnalyzeCurrentPlacements() {
 
-	//Only 3 and 4 player games are supported
-	if ga.playerCount < 3 {
+	//Only 2, 3 and 4 player games are supported
+	if ga.playerCount < 2 {
 		return
+	}
+
+	offset := Offset
+	placementReference := PlacementReference
+	placementHashes := PlacementHashes
+
+	if ga.playerCount == 2 {
+		offset = Offset2P
+		placementReference = PlacementReference2Player
+		placementHashes = PlacementHashes2P
 	}
 
 	newPlacements := ga.placements
 	change := false
 
 	for i := 0; i < ga.playerCount; i++ {
-		refPx := PlacementReference[i]
+		refPx := placementReference[i]
 
-		cropped := ga.capture.Crop(refPx.X-12, refPx.Y-10, refPx.X+20, refPx.Y+40)
+		cropped := ga.capture.Crop(refPx.X+offset.X1, refPx.Y+offset.Y1, refPx.X+offset.X2, refPx.Y+offset.Y2)
 		placeMat := gocv.NewMat()
 
 		gocv.CvtColor(*cropped, &placeMat, gocv.ColorBGRToGray)
